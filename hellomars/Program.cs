de34cbe14be9683c1b0ebf3dc55601e81ced2a23
tas <tasqur9@gmail.com>
2022-11-14 21:25:50 +0100

    firstCSharpCode

---
+++ hellomars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace snallWeeks_lab_firstDotNetApp
+{
+    class Program
+  {
+    private static string greet(string namePassedIn)
+    {
+      return "Welcome to SALT, "+ namePassedIn;
+    }
+    static void Main(string[] args)
+    {
+      Console.Write("What is your name? ");
+      var name = Console.ReadLine();
+
+      var greeting = greet(name);
+
+      Console.WriteLine(greeting);
+    }
+  }
+}
